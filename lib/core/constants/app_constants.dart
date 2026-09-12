acdd5e3293f864f6b6d10663d30d8294f9dc9514
erikDev <141310212+erik-dev-8386@users.noreply.github.com>
2026-09-12 10:38:21 +0700

    add
    add

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String appName = 'Mo Nailify Project';
+//  static const String baseUrl = 'http://10.0.2.2:5004';
+  // Android emulator -> host HTTP backend
+  static const String baseUrl = 'https://nailify-be.onrender.com/';
+  static const String apiVersion = '/api';
+  static const String authTokenKey = 'auth_token';
+  static const Duration connectTimeout = Duration(seconds: 60);
+  static const Duration receiveTimeout = Duration(seconds: 60);
+}
